bdc0168d289b0e7bc7cefe3f7ea7afacb3485bef
jorvixsky <jorvixsky@proton.me>
2025-07-07 21:39:27 +0200

    Now use mnemonics

--- backend/routes/wallet.ts
+++ backend/routes/wallet.ts
@@ -1,7 +1,7 @@
 import { Hono } from 'hono'
 import { verify } from 'hono/jwt'
 import { Client } from "https://deno.land/x/postgres@v0.17.0/mod.ts";
-import { generatePrivateKey, privateKeyToAccount } from 'npm:viem/accounts'
+import { generateMnemonic, privateKeyToAccount } from 'npm:viem/accounts'
 
 interface Wallet {
     phone_number: string;
@@ -36,18 +36,18 @@ wallet.get('/create', async (c) => {
         `, [sub])
 
         if (result.rows.length !== 0) {
-            return c.json({ error: 'User already exists' }, 400)
+            return c.json({ error: 'Account has already been created' }, 400)
         }
 
-        const privateKey = generatePrivateKey()
+        const mnemonic = generateMnemonic()
+        const address = mnemonicToAccount(mnemonic).address
 
-        const publicKey = privateKeyToAccount(privateKey).address
 
         await db.queryObject(`
-            INSERT INTO public.wallets (phone_number, private_key, public_key) VALUES ($1, $2, $3)
-        `, [sub, privateKey, publicKey])
+            INSERT INTO public.wallets (phone_number, mnemonic, address) VALUES ($1, $2, $3)
+        `, [sub, mnemonic, address])
 
-        return c.json({ publicKey })
+        return c.json({ address })
     } catch (error) {
         console.error(error)
         return c.json({ error: 'Internal server error' }, 500)
